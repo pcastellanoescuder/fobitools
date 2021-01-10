df9faa5ec83f92c9cfa098a16a8dd1ab190a089a
Pol Castellano Escuder <polcaes@gmail.com>
2021-01-10 21:34:41 +0100

    id_convert done

--- R/id_convert.R
+++ R/id_convert.R
@@ -1,62 +1,51 @@
 
-#' Convert Metabolite IDs Using FOBI Annotations
+#' Convert Metabolite Identifiers
 #'
-#' @description This function can convert metabolite IDs to the other ID types available in FOBI. Note that the input vector can be a combination of different IDs. 
+#' @description This function can convert metabolite identifiers to other available IDs in FOBI. Input vector can be a combination of different IDs. 
 #'
-#' @param ids Character vector with metabolite IDs to convert. Input ID types can be FOBI, raw metabolite names (used in FOBI), HMDB, KEGG, PubChemCID, InChIKey, InChICode, ChemSpider or a combination of them.
-#' @param to Target ID type. If possible, metabolites will be converted to this ID type. Options are "FOBI" (default), "metaboliteNames", "HMDB", "KEGG", "PubChemCID", "InChIKey", "InChICode" and "ChemSpider".
-#' @param stable_version Logical. If it's set to TRUE (default), the function will use an stable version of FOBI. If not, the function will use the `fobi.obo` file from GitHub (\url{https://github.com/pcastellanoescuder/FoodBiomarkerOntology}).
+#' @param ids A vector with metabolite IDs to convert. Input ID types can be FOBI, raw metabolite names (used in FOBI), HMDB, KEGG, PubChemCID, InChIKey, InChICode, ChemSpider, and a combination of them.
+#' @param to Target ID type. If possible, metabolites will be converted to this ID type. Options are "FOBI" (default), "metaboliteNames", "HMDB", "KEGG", "PubChemCID", "InChIKey", "InChICode", and "ChemSpider".
 #' 
 #' @export
 #'
-#' @return A data frame with input IDs and converted IDs.
+#' @return A tibble with input IDs and converted IDs.
+#' @references Pol Castellano-Escuder, Raúl González-Domínguez, David S Wishart, Cristina Andrés-Lacueva, Alex Sánchez-Pla, FOBI: an ontology to represent food intake data and associate it with metabolomic data, Database, Volume 2020, 2020, baaa033, https://doi.org/10.1093/databa/baaa033.
 #' @author Pol Castellano-Escuder
 #'
 #' @importFrom magrittr %>%
-#' @importFrom dplyr mutate select rename filter mutate_all matches vars select_at filter_all any_vars
+#' @importFrom dplyr mutate select rename filter mutate_all matches vars select_at filter_all any_vars as_tibble relocate last_col
 #' @importFrom stringr regex str_replace_all str_trim
-#' @importFrom crayon red
-#' @importFrom clisymbols symbol
 id_convert <- function(ids,
-                       to = "FOBI",
-                       stable_version = TRUE){
+                       to = "FOBI"){
   
   if (!(to %in% c("FOBI", "PubChemCID", "KEGG", "InChIKey", "InChICode", "ChemSpider", "metaboliteNames", "HMDB"))) {
-    stop(crayon::red(clisymbols::symbol$cross, "Select one valid ID type!"))
+    stop("Select a valid ID type!")
   }
   
-  if(!stable_version){
-    
-    fobi <- parse_fobi()
-    
-    idmap <- fobi %>%
-      filter(!BiomarkerOf == "NULL") %>%
-      select(name, FOBI, HMDB, KEGG, PubChemCID, InChIKey, InChICode, ChemSpider) %>%
-      mutate_all(as.character) %>%
-      mutate(HMDB = str_replace_all(HMDB, regex("( ).*"), regex("\\1")),
-             KEGG = str_replace_all(KEGG, regex("( ).*"), regex("\\1")),
-             PubChemCID = str_replace_all(PubChemCID, regex("( ).*"), regex("\\1")),
-             ChemSpider = str_replace_all(ChemSpider, regex("( ).*"), regex("\\1"))) %>%
-      rename(metaboliteNames = name) %>%
-      mutate_all(~ ifelse(. == "NULL", NA, .)) %>%
-      mutate_all(~ stringr::str_trim(.)) %>%
-      filter(!duplicated(metaboliteNames))
-    
-    }
+  fobi <- parse_fobi()
   
-  else {
-    
-    data("idmap")
-    
-  }
-  
+  idmap <- fobi %>%
+    filter(!BiomarkerOf == "NULL") %>%
+    select(name, FOBI, HMDB, KEGG, PubChemCID, InChIKey, InChICode, ChemSpider) %>%
+    mutate_all(as.character) %>%
+    mutate(HMDB = str_replace_all(HMDB, regex("( ).*"), regex("\\1")),
+           KEGG = str_replace_all(KEGG, regex("( ).*"), regex("\\1")),
+           PubChemCID = str_replace_all(PubChemCID, regex("( ).*"), regex("\\1")),
+           ChemSpider = str_replace_all(ChemSpider, regex("( ).*"), regex("\\1"))) %>%
+    rename(metaboliteNames = name) %>%
+    mutate_all(~ ifelse(. == "NULL", NA, .)) %>%
+    mutate_all(~ stringr::str_trim(.)) %>%
+    filter(!duplicated(metaboliteNames))
+
   ids <- ids[!is.na(ids)]
   from <- data.frame(presence = apply(idmap, 2, function(x) any(ids %in% x))) %>%
     filter(presence == "TRUE")
     
   conversion <- idmap %>%
     select_at(vars(dplyr::matches(rownames(from)), dplyr::matches(to))) %>%
-    filter_all(any_vars(. %in% ids))
+    filter_all(any_vars(. %in% ids)) %>%
+    relocate(last_col()) %>%
+    as_tibble()
   
   return(conversion)
   
--- tests/testthat/test-id_convert.R
+++ tests/testthat/test-id_convert.R
@@ -7,30 +7,23 @@ test_that("id_convert works", {
     
   ##
     
-  tictoc::tic()
   a <- fobitools::id_convert(ids, to = "FOBI")
-  time1 <- tictoc::toc()
-  
   b <- fobitools::id_convert(ids, to = "PubChemCID")
-  
-  tictoc::tic()
-  c <- fobitools::id_convert(ids, to = "FOBI", stable_version = FALSE)
-  time2 <- tictoc::toc()
-  
-  ##
-  
-  expect_false((time1$toc-time1$tic) == (time2$toc-time2$tic))
-  expect_true((time1$toc-time1$tic) < (time2$toc-time2$tic))
   
   ##
   
   expect_equal(ncol(a), 4)
   expect_equal(ncol(b), 4)
-  expect_equal(ncol(c), 4)
+  
+  ##
+  
+  expect_true(class(a)[2] == "tbl")
+  expect_true(class(b)[2] == "tbl")
+  
+  ##
   
   expect_equal(nrow(a), length(ids) - nas)
   expect_equal(nrow(b), length(ids) - nas)
-  expect_equal(nrow(c), length(ids) - nas)
   
   ##
   
